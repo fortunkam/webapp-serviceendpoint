706c9b5d8d6d7a8300077482b257d9340fea1eee
Matthew Fortunka <mafortun@microsoft.com>
2020-06-10 10:09:47 +0100

    changed firewall to NAT Gateway.

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,17 +1,17 @@
-resource "azurerm_virtual_network" "hub" {
-  name                = local.vnet_hub_name
-  location            = azurerm_resource_group.hub.location
-  resource_group_name = azurerm_resource_group.hub.name
-  address_space       = [local.vnet_hub_iprange]
-  dns_servers         = [local.dns_server_private_ip]
-}
+# resource "azurerm_virtual_network" "hub" {
+#   name                = local.vnet_hub_name
+#   location            = azurerm_resource_group.hub.location
+#   resource_group_name = azurerm_resource_group.hub.name
+#   address_space       = [local.vnet_hub_iprange]
+#   dns_servers         = [local.dns_server_private_ip]
+# }
 
-resource "azurerm_subnet" "firewall" {
-  name                 = local.firewall_subnet
-  resource_group_name  = azurerm_resource_group.hub.name
-  virtual_network_name = azurerm_virtual_network.hub.name
-  address_prefix       = local.firewall_subnet_iprange
-}
+# resource "azurerm_subnet" "firewall" {
+#   name                 = local.firewall_subnet
+#   resource_group_name  = azurerm_resource_group.hub.name
+#   virtual_network_name = azurerm_virtual_network.hub.name
+#   address_prefix       = local.firewall_subnet_iprange
+# }
 
 
 resource "azurerm_virtual_network" "spoke" {
@@ -54,35 +54,35 @@ resource "azurerm_subnet" "data" {
   service_endpoints = [ "Microsoft.Storage" ]
 }
 
-resource "azurerm_virtual_network_peering" "hubtospoke" {
-  name                      = local.hub_to_spoke_vnet_peer
-  resource_group_name       = azurerm_resource_group.hub.name
-  virtual_network_name      = azurerm_virtual_network.hub.name
-  remote_virtual_network_id = azurerm_virtual_network.spoke.id
-}
+# resource "azurerm_virtual_network_peering" "hubtospoke" {
+#   name                      = local.hub_to_spoke_vnet_peer
+#   resource_group_name       = azurerm_resource_group.hub.name
+#   virtual_network_name      = azurerm_virtual_network.hub.name
+#   remote_virtual_network_id = azurerm_virtual_network.spoke.id
+# }
 
-resource "azurerm_virtual_network_peering" "spoketohub" {
-  name                      = local.spoke_to_hub_vnet_peer
-  resource_group_name       = azurerm_resource_group.spoke.name
-  virtual_network_name      = azurerm_virtual_network.spoke.name
-  remote_virtual_network_id = azurerm_virtual_network.hub.id
-}
+# resource "azurerm_virtual_network_peering" "spoketohub" {
+#   name                      = local.spoke_to_hub_vnet_peer
+#   resource_group_name       = azurerm_resource_group.spoke.name
+#   virtual_network_name      = azurerm_virtual_network.spoke.name
+#   remote_virtual_network_id = azurerm_virtual_network.hub.id
+# }
 
-resource "azurerm_route_table" "web" {
-  name                          = local.firewall_route_table_name
-  location                      = azurerm_resource_group.spoke.location
-  resource_group_name           = azurerm_resource_group.spoke.name
-  disable_bgp_route_propagation = false
+# resource "azurerm_route_table" "web" {
+#   name                          = local.firewall_route_table_name
+#   location                      = azurerm_resource_group.spoke.location
+#   resource_group_name           = azurerm_resource_group.spoke.name
+#   disable_bgp_route_propagation = false
 
-  route {
-    name           = local.firewall_route_name
-    address_prefix = "0.0.0.0/0"
-    next_hop_type  = "VirtualAppliance"
-    next_hop_in_ip_address = azurerm_firewall.firewall.ip_configuration[0].private_ip_address
-  }
-}
+#   route {
+#     name           = local.firewall_route_name
+#     address_prefix = "0.0.0.0/0"
+#     next_hop_type  = "VirtualAppliance"
+#     next_hop_in_ip_address = azurerm_firewall.firewall.ip_configuration[0].private_ip_address
+#   }
+# }
 
-resource "azurerm_subnet_route_table_association" "web_to_dns" {
-  subnet_id      = azurerm_subnet.web.id
-  route_table_id = azurerm_route_table.web.id
-}
+# resource "azurerm_subnet_route_table_association" "web_to_dns" {
+#   subnet_id      = azurerm_subnet.web.id
+#   route_table_id = azurerm_route_table.web.id
+# }
